f2d460e205ee11d5ef60a0a2356bb30a0fe524f2
Christian Nielsen <120013949+HrSejer@users.noreply.github.com>
2024-06-21 08:27:35 +0200

    Subnet Calc works

--- TalSystemConverter/View/IpSubnetCalcView.xaml.cs
+++ TalSystemConverter/View/IpSubnetCalcView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,5 +25,110 @@ namespace TalSystemConverter.View
         {
             InitializeComponent();
         }
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var ipAddress = IPAddress.Parse(IpAddressTextBox.Text);
+                var netMask = int.Parse(NetMaskTextBox.Text.Replace("/", ""));
+
+                // Beregn subnetmask
+                var subnetMask = GetSubnetMask(netMask);
+
+                // Beregn networkaddress
+                var networkAddress = GetNetworkAddress(ipAddress, subnetMask);
+
+                // Beregn broadcastaddress
+                var broadcastAddress = GetBroadcastAddress(networkAddress, subnetMask);
+
+                // Beregn hostrange
+                var (firstHost, lastHost) = GetHostRange(networkAddress, broadcastAddress);
+
+                // Beregn nummer af hosts
+                var numberOfHosts = (int)Math.Pow(2, 32 - netMask) - 2;
+
+                // Display resultater
+                SubnetMaskTextBox.Text = subnetMask.ToString();
+                NetworkAddressTextBox.Text = networkAddress.ToString();
+                RangeTextBox.Text = $"{firstHost} - {lastHost}";
+                NumberOfHostsTextBox.Text = numberOfHosts.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private IPAddress GetSubnetMask(int netMask)
+        {
+            // Sikre netMask er valid
+            if (netMask < 0 || netMask > 32)
+            {
+                throw new ArgumentException("Invalid netmask værdi.");
+            }
+
+            byte[] maskBytes = new byte[4];
+
+            // Beregn subnet mask bytes
+            for (int i = 0; i < 4; i++)
+            {
+                if (netMask >= 8)
+                {
+                    maskBytes[i] = 255; // 8 bits sat til 1 (255 i decimal)
+                    netMask -= 8;
+                }
+                else if (netMask > 0)
+                {
+                    maskBytes[i] = (byte)(255 << (8 - netMask)); // Bits tilovers sat til 1
+                    netMask = 0;
+                }
+                else
+                {
+                    maskBytes[i] = 0; // Bits tilovers sat til 0
+                }
+            }
+
+            return new IPAddress(maskBytes);
+        }
+
+        private IPAddress GetNetworkAddress(IPAddress ipAddress, IPAddress subnetMask)
+        {
+            var ipBytes = ipAddress.GetAddressBytes();
+            var maskBytes = subnetMask.GetAddressBytes();
+            var networkBytes = new byte[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                networkBytes[i] = (byte)(ipBytes[i] & maskBytes[i]);
+            }
+
+            return new IPAddress(networkBytes);
+        }
+
+        private IPAddress GetBroadcastAddress(IPAddress networkAddress, IPAddress subnetMask)
+        {
+            var networkBytes = networkAddress.GetAddressBytes();
+            var maskBytes = subnetMask.GetAddressBytes();
+            var broadcastBytes = new byte[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                broadcastBytes[i] = (byte)(networkBytes[i] | ~maskBytes[i]);
+            }
+
+            return new IPAddress(broadcastBytes);
+        }
+
+        private (IPAddress firstHost, IPAddress lastHost) GetHostRange(IPAddress networkAddress, IPAddress broadcastAddress)
+        {
+            var networkBytes = networkAddress.GetAddressBytes();
+            var broadcastBytes = broadcastAddress.GetAddressBytes();
+
+            networkBytes[3]++;
+            broadcastBytes[3]--;
+
+            return (new IPAddress(networkBytes), new IPAddress(broadcastBytes));
+        }
+
     }
 }
